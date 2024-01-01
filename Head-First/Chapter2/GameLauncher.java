fe8b1903ecceb4c2d6aa2024b6c5aebfa16ca490
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-01 13:41:20 +0530

    Create GameLauncher.java

---
+++ Head-First/Chapter2/GameLauncher.java
@@ -0,0 +1,12 @@
+package demopkg2;
+
+public class GameLauncher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GuessGame gg = new GuessGame();
+		gg.startGame();
+
+	}
+
+}
