8103de818edf4ee12bea9ae47c6031199c4f9404
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-06 23:10:57 +0530

    Create PrintMatrixWaveform.java

---
+++ PrintMatrixWaveform.java
@@ -0,0 +1,16 @@
+//Pseudo code
+int row=n;
+int col=m;
+int[][] arr=new int[row][col];
+for(int i =0;i<row;i++){
+  if(i%2==0){
+    for(int j =0;j<col;j++){
+      System.out.print(arr[i][j]+" ");
+    }
+  }else{   
+      for(int j =col-1;j>=0;j--){
+        System.out.print(arr[i][j]+" ");
+      }    
+  }
+  System.out.println();
+}
