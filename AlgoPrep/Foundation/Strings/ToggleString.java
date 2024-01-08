4d511bd96c1dab79d52fc57fb30a768e9bcb31a0
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-08 23:29:49 +0530

    Create ToggleString.java

---
+++ AlgoPrep/Foundation/Strings/ToggleString.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        char[] ch=st.toCharArray();
+        for(int i =0;i<ch.length;i++){
+            if(ch[i]>=65 && ch[i]<=90){
+                ch[i]=(char)(ch[i]+32);
+            }else{
+                ch[i]=(char)(ch[i]-32);
+            }
+        }
+        System.out.println(ch);
+    }
+}
+
+//TC O(N)
+//SC O(1)
