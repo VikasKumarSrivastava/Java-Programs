0725af97299e629c537af73621eecbe20d39d681
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-11 23:51:16 +0530

    Create Fibonacci_number.java

---
+++ AlgoPrep/Foundation/Recursion/Fibonacci_number.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static int fib(int n){
+        if(n==0||n==1)
+            return n;
+        return fib(n-1)+fib(n-2);
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = fib(n);
+        System.out.println(ans);
+    }
+}
