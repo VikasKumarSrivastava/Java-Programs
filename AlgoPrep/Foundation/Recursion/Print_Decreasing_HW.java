cc2eafe94c1307a1e3b032ee15e34bb7cf3e52b6
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-11 23:54:58 +0530

    Create Print_Decreasing_HW.java

---
+++ AlgoPrep/Foundation/Recursion/Print_Decreasing_HW.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void printDec(int n){
+        if(n==0)
+            return;
+        System.out.println(n);
+        printDec(n-1);
+        
+    }
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printDec(n);
+    }
+}
