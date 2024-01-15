af929794f39a8f8efb3db3073c055c97e18665da
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-16 00:44:13 +0530

    Create binarySearch.java

---
+++ AlgoPrep/Foundation/BinarySearch/binarySearch.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static boolean search(int arr[],int k){
+        int lo=0;
+        int hi = arr.length-1;
+        while(lo<=hi){
+            int mid = lo +(hi-lo)/2;
+            if(arr[mid]==k)
+                return true;
+            else if(arr[mid]<k){
+                lo = mid+1;
+            }else{
+                hi = mid-1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i =0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int target = sc.nextInt();
+        boolean ans = search(arr,target);
+        System.out.println(ans);
+    }
+}
