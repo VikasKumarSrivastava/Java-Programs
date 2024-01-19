e666c73df8f063c1d2b2df0069f7efe87e307010
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-19 22:29:37 +0530

    Create PrintLL.java

---
+++ AlgoPrep/Foundation/LinkedList/PrintLL.java
@@ -0,0 +1,17 @@
+class Solution{
+  class Node{
+    int val;
+    Node next;
+    Node(int v){
+      val =v;
+      next = NULL:
+        }
+  }
+    public void static display(Node head){
+      Node temp = head;
+      while(temp!=NULL){
+        System.out.print(temp.data+" ");
+        temp = temp.next;
+      }
+  }
+}
