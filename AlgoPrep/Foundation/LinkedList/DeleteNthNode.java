8221a111c6b85aec28e2689ceda6e03cfc9a5cf0
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-20 00:16:41 +0530

    Create DeleteNthNode.java

---
+++ AlgoPrep/Foundation/LinkedList/DeleteNthNode.java
@@ -0,0 +1,41 @@
+//https://www.geeksforgeeks.org/problems/delete-a-node-in-single-linked-list/1
+/* Linklist node structure
+class Node
+{
+	int data;
+	Node next;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+/*You are required to complete below method*/
+class GfG
+{
+    Node deleteNode(Node head, int x)
+    {
+	    if (head == null){
+	        return head;
+	    }
+	    if(head.next == null && x>=2){
+	        return head;
+	    }
+	    if(x==1){
+	        return head.next;
+	    }
+	    
+	    int count =1;
+	    Node prev = null;
+	    Node temp = head;
+        while(count!=x){
+            prev = temp;
+            temp = temp.next;
+            count+=1;
+        }
+        prev.next = temp.next;
+        temp.next=null;
+        return head;
+    }
+}
