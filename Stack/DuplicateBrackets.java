1aed92025ffa96a1c33ee2d59eb0fd4af265e950
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-25 18:43:07 +0530

    Update DuplicateBrackets.java

--- Stack/DuplicateBrackets.java
+++ Stack/DuplicateBrackets.java
@@ -5,7 +5,6 @@ public class Main {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String str = br.readLine();
-
     Stack< Character> st = new Stack< >();
     for (int i = 0; i < str.length(); i++) {
       char ch = str.charAt(i);
