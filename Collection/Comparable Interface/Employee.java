20677975633b5f34da059be9978bdc08900e53d4
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-02-23 21:39:04 +0530

    Create Employee.java

---
+++ Collection/Comparable Interface/Employee.java
@@ -0,0 +1,20 @@
+public class Employee implements Comparable<Employee> {
+	
+	String name;
+	int age;
+	String country;
+	
+	public Employee(String name, int age, String country) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.country = country;
+	}
+
+	@Override
+	public int compareTo(Employee e){
+		return this.country.compareTo(e.country);
+	}
+
+
+}
