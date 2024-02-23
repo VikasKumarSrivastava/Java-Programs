2842b860557b22595d31ffc206419bc0a1968bf8
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-02-23 21:39:54 +0530

    Create ArrayListExercise.java

---
+++ Collection/Comparable Interface/ArrayListExercise.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListExercise {
+	
+	public static void main(String args[]) {
+		List<Employee> list = new ArrayList<>();
+		list.add(new Employee("Alex", 23, "USA"));
+		list.add(new Employee("Dave", 34, "India"));
+		list.add(new Employee("Carl", 21, "USA"));
+		list.add(new Employee("Joe", 56, "Russia"));
+		list.add(new Employee("Amit", 64, "China"));
+		list.add(new Employee("Ryan", 19, "Brazil"));
+
+		Collections.sort(list);
+
+		for(Employee e : list){
+			System.out.println(e.name+" "+e.country);
+		}
+		
+	}
+
+}
