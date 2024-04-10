d42125fc6a89a4977d7b9340247e71426f57d5b0
Vikas Srivastava <vikassrivastava@Vikass-MacBook-Air.local>
2024-04-10 20:05:34 +0530

    Spring Boot ex added

---
+++ SpringBootExamples/02-component-scanning/src/main/java/com/example/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(
+//		scanBasePackages = {"com.example.util",
+//							"com.example.springcoredemo.rest"}
+//)
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
---
+++ SpringBootExamples/04-qualifiers/src/main/java/com/example/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,11 @@
+package com.example.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast Bowling for 30 minutes";
+    }
+}
