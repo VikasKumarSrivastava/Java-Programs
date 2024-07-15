721c85d98a03fb023235302e789df5db82e582f3
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-07-15 23:24:58 +0530

    Update EvenOdd.java

--- AlgoPrep/Foundation/Bit_Manipulations/EvenOdd.java
+++ AlgoPrep/Foundation/Bit_Manipulations/EvenOdd.java
@@ -4,7 +4,6 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 public class Solution {
-
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
